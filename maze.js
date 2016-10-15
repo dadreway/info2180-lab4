f61d5a544bde6c66ef308ba1c2ecadb36b06c25c
dadreway <andrewshylton@gmail.com>
2016-10-14 19:44:45 -0500

    fixed minor bugs, .innerHTML still not working
    .innerHTML still not working, despite code for innerHTML being correct.

--- maze.js
+++ maze.js
@@ -8,17 +8,11 @@ var outOfBound = false;
 
 window.onload = function(){
 
-boundary = document.querySelectorAll(".boundary");
+boundary = document.querySelectorAll('.boundary');
 mazeBoundary = document.getElementById("maze");
-start =document.getElementById("start");
-end = document.getElementById("end");
-status = document.getElementById("status");
-
-start.onclick = reset;
-mazeBoundary.onmouseleave =anticheat;
-end.onmouseover = WinStatus;
-for (var i=0; i<boundary.length -1; i++) {
-boundary[i].onmouseover = highlight;
+start =document.getElementById('start');
+end = document.getElementById('end');
+status = document.getElementById('status');
 
 start.addEventListener("click", reset);
 mazeBoundary.addEventListener("mouseleave", anticheat);
@@ -26,6 +20,7 @@ end.addEventListener("mouseover", WinStatus);
 for (var i=0; i<boundary.length -1; i++ ){
 	boundary[i].addEventListener("mouseover", highlight);
 }
+
 }
 
 
@@ -34,7 +29,7 @@ function highlight(){
 	if(!win){
 		if(!outOfBound){
 			outOfBound = true;
-			status.innerHTML = "you lose!, Click on the S to play again.";
+			status.innerHTML= "you lose!, Click on the S to play again.";
 			for (var i=0; i<boundary.length -1; i++) {
 				boundary[i].setAttribute("class","boundary youlose");
 			}
@@ -47,14 +42,14 @@ function highlight(){
 function WinStatus(){
 		if(!outOfBound){
 			win = true;
-			status.innerHTML = 'You win! To play again click the S';
+			status.innerHTML= "You win! To play again click the S";
 		}
 }
 
 function reset(){
 	win = false;
 	outOfBound = false;
-	status.innerHTML = 'Move your mouse over the "S" to begin.';
+	status.innerHTML = "Move your mouse over the 'S' to begin.";
 	for (var i=0; i<boundary.length -1; i++) {
 				boundary[i].setAttribute("class","boundary boundary");
 			}
